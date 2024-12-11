c9f9980f8deb9fa6b7ba9ac63f253d0d2b755f79
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2024-12-11 12:07:59 +0100

    Logo

--- app/components/common/LogoDesktop.tsx
+++ app/components/common/LogoDesktop.tsx
@@ -3,17 +3,21 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "@/public/images/logo.png";
 
-const LogoAndName = ({ session }: any) => {
+const LogoDesktop = ({ session }: any) => {
     return (
         <>
             <Link
                 href={session?.user?.isAdmin ? "/admin" : "/"}
                 className="relative min-h-12 min-w-12 flex flex-row justify-start items-center content-start"
             >
-                <Image src={logo} alt="Logo" className="h-12 w-auto" />
+                <Image 
+                    src={logo} 
+                    alt="Logo" 
+                    className="h-12 w-auto" 
+                />
             </Link>
         </>
     );
 };
 
-export default LogoAndName;
+export default LogoDesktop;
--- app/components/navigation/index.tsx
+++ app/components/navigation/index.tsx
@@ -1,29 +1,25 @@
 "use client";
 
 import React from "react";
-import LogoAndName from "@/app/components/common/LogoAndName";
+import LogoDesktop from "@/app/components/common/LogoDesktop";
 import DropDownMenu from "@/app/components/navigation/DropdownMenu";
 import LogoMob from "@/app/components/common/LogoMob";
 import MenuItems from "@/app/components/navigation/MenuItems";
-import ButtonLoginArrow from "@/app/components/common/ButtonLoginArrow";
 import LogoutButtonArrow from "@/app/components/common/ButtonLogoutArrow";
-import { usePathname, useRouter } from "next/navigation";
 import UserIcon from "@/app/components/common/UserIcon";
 import Arrow from "@/app/components/common/Arrow";
 
 const Navigation = ({ session }: any) => {
-    const pathname = usePathname();
-    const router = useRouter();
-    const lang = pathname.split("/")[1];
-
     return (
         <>
             {/* Desktop Navigation */}
             <div className="md:hidden sm:hidden flex flex-row justify-between items-center w-full h-20 fixed top-0 left-0 z-100 pt-4 pb-4 pl-space2 pr-space2 bg-color1-transparent border-b-2">
                 <div>
-                    <LogoAndName session={session} />
+                    <LogoDesktop session={session} />
                 </div>
+
                 <MenuItems session={session} />
+
                 {session && (
                     <div className=" min-w-[42px]">
                         <UserIcon></UserIcon>
