eba43899d8ca50c96162edd868e0674898b74b07
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2025-01-27 15:03:15 +0100

    Update Card.tsx

--- app/components/common/Card.tsx
+++ app/components/common/Card.tsx
@@ -13,9 +13,9 @@ const Card = ({ customStyle = "", header = "Header", children }: Properties): JS
         <div
             id="container"
             // Allow it to receive a custom style
-            className={`p-space3 min-h-[50%] bg-color0 rounded shadow-[8px_8px_8px_8px_rgba(0,0,0,0.2)] flex flex-col justify-start items-start ${customStyle}`}
+            className={`p-space3 min-h-[550px] bg-color0 rounded shadow-[8px_8px_8px_8px_rgba(0,0,0,0.2)] flex flex-col justify-start items-start ${customStyle}`}
         >
-            <h1 className="py-space1 w-full text-center text-color8 font-bold bg-color7 rounded text-3xl">{header}</h1>
+            <h1 className="py-space2 w-full text-center text-color8 font-bold bg-color7 rounded text-3xl">{header}</h1>
 
             <div id="children" className="pt-space2 flex flex-col justify-start items-start ">
                 {children}
