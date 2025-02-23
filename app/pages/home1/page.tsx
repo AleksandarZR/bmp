03b5a20c61f29ae762fe8aba3c8e19faa72dd361
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2025-02-23 18:59:04 +0100

    a

--- app/pages/home1/page.tsx
+++ app/pages/home1/page.tsx
@@ -12,32 +12,6 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-    const [activeLogo, setActiveLogo] = useState(logo);
-    const [count, setCount] = useState(1);
-    const logos = [logo3, logo2, logo];
-
-    useEffect(() => {
-        let timer = setTimeout(() => {
-            if (count === 1) {
-                setActiveLogo(logo3);
-            }
-            if (count === 2) {
-                setActiveLogo(logo2);
-            }
-            if (count === 3) {
-                setActiveLogo(logo);
-            }
-            if (count > 5) {
-                setCount(0);
-            } else {
-                setCount((count) => count + 1);
-            }
-        }, 1000);
-
-        // Some effects require cleanup to reduce memory leaks. Timeouts, subscriptions, event listeners
-        return () => clearTimeout(timer);
-    }, [count, activeLogo]);
-
     return (
         <>
             <video
@@ -49,148 +23,33 @@ export default function Home() {
                 loop
             ></video>
 
-            <main className="w-screen max-w-full min-h-screen flex flex-col justify-start items-center overflow-x-hidden z-20">
+            <main className="w-screen max-w-full min-h-screen flex flex-col justify-start items-center overflow-x-hidden">
                 <div
                     id="view1"
-                    className="pt-[calc(var(--navheight)+var(--space5))] sm:pt-[calc(var(--navheight)+var(--space22))] px-space5 pb-space5 min-w-screen min-h-screen flex flex-col justify-between items-center sm:items-start overflow-x-hidden z-20"
+                    className="pt-[calc(var(--navheight)+var(--space5))] sm:pt-[calc(var(--navheight)+var(--space22))] px-space5 pb-space5 min-w-screen min-h-screen flex flex-col justify-between items-center sm:items-start overflow-x-hidden"
                 >
-                    {/* <motion.div
-                        id="imageContainer"
-                        className="fixed lg:h-[50%] w-[auto] bg-color1 shadow-[8px_8px_8px_8px_rgba(0,0,0,0.2)] mt-[-210px] px-space5 py-space5 flex flex-col items-center justify-center rounded-xl"
-                    >
-                        <Image
-                            src={activeLogo}
-                            width={0}
-                            height={0}
-                            className="h-full w-full"
-                            alt="Logo"
-                        ></Image>
-                    </motion.div> */}
-
-                    <div className="w-full flex flex-col justify-center items-center text-5xl text-color0 text-center z-20">
-                        <div className="text-color7 bg-black">
+                    <div className="w-full flex flex-col justify-center items-center text-5xl text-color7 text-center z-20">
+                        <div className={styles.textWithBorder}>
                             Moderately Awesome Developers
                         </div>
                     </div>
 
-                    <div className="w-full flex flex-col justify-center items-center text-2xl text-color0 text-center z-20">
-                        <div className="text-color7 drop-shadow-md bg-color1">
+                    <div className="w-full flex flex-col justify-center items-center text-2xl text-color7 text-center z-20">
+                        <div className={styles.textWithBorder}>
                             <span className="text-color8">42computing</span> is
                             a deep tech research and development company.
                         </div>
-                        <div className="text-color7 bg-black">
+                        <div className={styles.textWithBorder}>
                             Our vision is to build superior solutions for
                             real-world problems using blockchain, distributed
                             systems, artificial intelligence, computer vision,
                             and AR/VR.
                         </div>
-                        <div className="text-color7 bg-black">
+                        <div className={styles.textWithBorder}>
                             We use cutting-edge knowledge to help organizations
                             augment and transform their business processes.
                         </div>
                     </div>
-                </div>
-
-                <div
-                    id="view2"
-                    className="pt-[calc(var(--navheight)+2*var(--space5))] w-screen max-w-full lg:h-screen grid grid-rows-3 grid-cols-2 gap-space5 text-color7 text-2xl sm:text-xl"
-                >
-                    <div
-                        id="11"
-                        className="flex flex-col justify-center items-end"
-                    >
-                        <CoveredImageFlexible
-                            customStyle="max-h-[100%] bg-white"
-                            imagePath="/images/consulting.png"
-                        />
-                    </div>
-
-                    <motion.div
-                        id="12"
-                        className="flex flex-col justify-around items-start z-20"
-                        initial={{ opacity: 0 }}
-                        whileInView={{ opacity: 1 }}
-                        transition={{
-                            duration: 5,
-                            delay: 0.75,
-                            ease: "easeInOut",
-                        }}
-                        viewport={{ once: false }}
-                    >
-                        <h1 className="text-3xl text-color8 font-bold bg-black">
-                            Consulting
-                        </h1>
-                        <div className={styles.textWithBorder}>
-                            We offer strategic architecting and consulting, as
-                            well as innovation workshops. We provide technical
-                            leadership and help to determine bounds of what’s
-                            currently technically possible.
-                        </div>
-                    </motion.div>
-
-                    <motion.div
-                        id="21"
-                        className="flex flex-col justify-around items-end z-20"
-                        initial={{ opacity: 0 }}
-                        whileInView={{ opacity: 1 }}
-                        transition={{
-                            duration: 5,
-                            delay: 0.75,
-                            ease: "easeInOut",
-                        }}
-                        viewport={{ once: false }}
-                    >
-                        <h1 className="text-3xl text-color8 font-bold">
-                            Research & Development
-                        </h1>
-                        <div className={styles.textWithBorder}>
-                            We perform state-of-the-art research leading to
-                            feasibility studies and rapid software prototype
-                            design and development.
-                        </div>
-                    </motion.div>
-
-                    <div
-                        id="22"
-                        className="flex flex-col justify-center items-start"
-                    >
-                        <CoveredImageFlexible
-                            customStyle="max-h-[100%] bg-color0"
-                            imagePath="/images/rnd.png"
-                        />
-                    </div>
-
-                    <div
-                        id="31"
-                        className="flex flex-col justify-center items-end"
-                    >
-                        <CoveredImageFlexible
-                            customStyle="max-h-[100%] w-[auto] bg-white"
-                            imagePath="/images/outsorcing.png"
-                        />
-                    </div>
-
-                    <motion.div
-                        id="32"
-                        className="flex flex-col justify-around items-start z-20"
-                        initial={{ opacity: 0 }}
-                        whileInView={{ opacity: 1 }}
-                        transition={{
-                            duration: 5,
-                            delay: 0.75,
-                            ease: "easeInOut",
-                        }}
-                        viewport={{ once: false }}
-                    >
-                        <h1 className="text-3xl text-color8 font-bold drop-shadow-2xl">
-                            Outsourcing
-                        </h1>
-                        <div className={styles.textWithBorder}>
-                            We build custom software solutions using blockchain,
-                            machine learning, and AR/VR and integrate them with
-                            client's existing IT systems.
-                        </div>
-                    </motion.div>
                 </div>
             </main>
         </>
